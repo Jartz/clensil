0525baad65cedf523c55521e04396a0e879f86e2
Jartz <ing.julian.ramos@gmail.com>
2018-10-19 18:46:27 -0500

    login firebase google

--- CLESIL/controller/Login/LoginController.swift
+++ CLESIL/controller/Login/LoginController.swift
@@ -7,21 +7,34 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LoginController: UIViewController{
+class LoginController: UIViewController, GIDSignInUIDelegate{
     override func viewDidLoad(){
-     super.viewDidLoad()
-    navigationController?.setNavigationBarHidden(true, animated: true) // desaparece view controller
-    view.backgroundColor = .white
-    
+        super.viewDidLoad()
+        
+        navigationController?.setNavigationBarHidden(true, animated: true) // desaparece view controller
+        view.backgroundColor = .white
+        
         viewMain()
-
-        /*
-        let blurEffect = UIBlurEffect(style: .light)
-        let blurView = UIVisualEffectView(effect:blurEffect)
-        blurView.frame = background_image.bounds
-        background_image.addSubview(blurView)
-        */
+        actionImages()
+        validateUserCurrent()
+        
+        Auth.auth().addStateDidChangeListener() { (auth, firebaseUser) in
+            if let firebaseUser = firebaseUser {
+                self.goCategorys()
+            } else {
+                print("not logged int")
+            }
+        }
+        
+          /*
+         let blurEffect = UIBlurEffect(style: .light)
+         let blurView = UIVisualEffectView(effect:blurEffect)
+         blurView.frame = background_image.bounds
+         background_image.addSubview(blurView)
+         */
         
     }
     
@@ -34,9 +47,11 @@ class LoginController: UIViewController{
         container.addSubview(lbSubTitle)
         container.addSubview(lbLogin)
         container.addSubview(ivButtonLogin)
-        
         setupConstraint()
-        
+    }
+    
+    func actionImages(){
+     ivButtonLogin.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(singGoogle)))
     }
     
     // ======== CONFIG ==========
@@ -44,11 +59,11 @@ class LoginController: UIViewController{
     let container: UIView = {
         let cont = UIView()
         cont.translatesAutoresizingMaskIntoConstraints = false
-       //cont.backgroundColor = .red
+        //cont.backgroundColor = .yellow
         return cont
     }()
     
-
+    
     var background_image : UIImageView = {
         let iv = UIImageView()
         iv.image = UIImage(named: "bg_login")
@@ -60,9 +75,6 @@ class LoginController: UIViewController{
         return iv
     }()
     
-
-     
- 
     
     let lbTitle : UILabel = {
         let lb = UILabel()
@@ -80,7 +92,7 @@ class LoginController: UIViewController{
         let lb = UILabel()
         lb.text = "CLEAR SERVICE INTERNATIONAL"
         lb.textColor = .white
-       // lb.backgroundColor = .blue
+        // lb.backgroundColor = .blue
         lb.font = lb.font.withSize(20)
         lb.textAlignment = .center
         lb.translatesAutoresizingMaskIntoConstraints = false
@@ -98,45 +110,47 @@ class LoginController: UIViewController{
         return lb
     }()
     
-    var ivButtonLogin : UIImageView = {
+    let ivButtonLogin : UIImageView = {
         let iv = UIImageView()
         iv.image = UIImage(named: "google2")
         iv.contentMode = .scaleToFill
-        iv.clipsToBounds = true
         iv.layer.masksToBounds = true
-        iv.isUserInteractionEnabled = false
+        iv.isUserInteractionEnabled = true
         iv.translatesAutoresizingMaskIntoConstraints = false
         return iv
     }()
     
-    /*
-    let tvMail : UITextField = {
-        let tv = UITextField()
-        tv.text = ""
-        tv.placeholder = "Correo Electronico"
-        tv.leftView = UIImageView(image: UIImage(named: "usuario"))
-        tv.leftViewMode = .always
-        tv.borderStyle = UITextBorderStyle.roundedRect
-        tv.autocorrectionType = UITextAutocorrectionType.no
-        tv.keyboardType = UIKeyboardType.default
-        tv.returnKeyType = UIReturnKeyType.done
-        tv.clearButtonMode = UITextFieldViewMode.whileEditing;
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        return tv
+    lazy var btnCategorys : UIButton = {
+        let btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("go to categorys", for: .normal)
+        btn.addTarget(self, action: #selector(goCategorys), for: .touchUpInside)
+        btn.isUserInteractionEnabled = true
+        return btn
     }()
- */
-     
-     lazy var btnCategorys : UIButton = {
-     let btn = UIButton(type: .system)
-     btn.translatesAutoresizingMaskIntoConstraints = false
-     btn.setTitle("go to categorys", for: .normal)
-     btn.addTarget(self, action: #selector(goCategorys), for: .touchUpInside)
-     btn.isUserInteractionEnabled = true
-     return btn
-     }()
-
     
-     // ======== POSITION ==========
+   
+    /*
+     let tvMail : UITextField = {
+     let tv = UITextField()
+     tv.text = ""
+     tv.placeholder = "Correo Electronico"
+     tv.leftView = UIImageView(image: UIImage(named: "usuario"))
+     tv.leftViewMode = .always
+     tv.borderStyle = UITextBorderStyle.roundedRect
+     tv.autocorrectionType = UITextAutocorrectionType.no
+     tv.keyboardType = UIKeyboardType.default
+     tv.returnKeyType = UIReturnKeyType.done
+     tv.clearButtonMode = UITextFieldViewMode.whileEditing;
+     tv.translatesAutoresizingMaskIntoConstraints = false
+     return tv
+     }()
+     */
+    
+    
+    
+    
+    // ======== POSITION ==========
     
     func setupConstraint(){
         
@@ -154,7 +168,7 @@ class LoginController: UIViewController{
         btnCategorys.leftAnchor.constraint(equalTo: container.leftAnchor).isActive = true
         btnCategorys.rightAnchor.constraint(equalTo: container.rightAnchor).isActive = true
         
-    
+        
         lbTitle.topAnchor.constraint(equalTo: container.topAnchor, constant: 0).isActive = true
         lbTitle.leftAnchor.constraint(equalTo: container.leftAnchor).isActive = true
         lbTitle.rightAnchor.constraint(equalTo: container.rightAnchor).isActive = true
@@ -167,16 +181,21 @@ class LoginController: UIViewController{
         lbLogin.leftAnchor.constraint(equalTo: container.leftAnchor).isActive = true
         lbLogin.rightAnchor.constraint(equalTo: container.rightAnchor).isActive = true
         
-       ivButtonLogin.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        ivButtonLogin.heightAnchor.constraint(equalToConstant: 48).isActive = true
         ivButtonLogin.widthAnchor.constraint(equalToConstant: 48).isActive = true
         ivButtonLogin.topAnchor.constraint(equalTo: lbLogin.bottomAnchor, constant : 37).isActive = true
-       ivButtonLogin.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        ivButtonLogin.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
         
-        
-        
-        
-        
-        
+       
+    }
+    
+    func validateUserCurrent(){
+        let user = Auth.auth().currentUser
+        if let user = user {
+            let email = user.email
+            print(email as! String)
+            goCategorys()
+        }
     }
     
     
@@ -187,7 +206,12 @@ class LoginController: UIViewController{
         self.present(navigationController, animated: true, completion: nil)
     }
     
+ 
     
-
+    @objc func singGoogle(sender: UITapGestureRecognizer){
+        print("Tap Gesture recognized")
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
+    }
     
 }
